61cff6ad8e2c1d8a3100822826e3d362108c2bb0
eversoluna <phan.powray@outlook.com>
2021-08-29 14:49:45 +0700

    set deployment params for script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,61 +1,55 @@
-// This is a script for deploying your contracts. You can adapt it to deploy
-// yours, or create new ones.
-
-const path = require("path");
-
+// scripts/deploy.js
 async function main() {
-  // This is just a convenience check
-  if (network.name === "hardhat") {
-    console.warn(
-      "You are trying to deploy a contract to the Hardhat Network, which" +
-        "gets automatically created and destroyed every time. Use the Hardhat" +
-        " option '--network localhost'"
+    // Fetch the signers
+    const [deployer] = await ethers.getSigners();
+    console.log("Deploying contracts with the account:", deployer.address);
+
+    // Set contract constructor parameters
+    const ticketPrice = ethers.utils.parseEther("0.1"); // 0.1 ETH ticket price
+    const housePercentage = 10; // 10% of prize goes to the house
+    const maxWinners = 3;
+    const vrfCoordinator = process.env.VRF_COORDINATOR;
+    const linkToken = process.env.LINK_TOKEN;
+    const keyHash = process.env.CHAINLINK_KEY_HASH;
+    const fee = process.env.CHAINLINK_FEE;
+
+    // Log out the parameters to confirm they're being read correctly
+    console.log(
+      `Deploying contract with:
+      Ticket Price: ${ticketPrice} ETH
+      House Percentage: ${housePercentage}%
+      Max Winners: ${maxWinners}
+      VRF Coordinator: ${vrfCoordinator}
+      LINK Token: ${linkToken}
+      Key Hash: ${keyHash}
+      Fee: ${fee}`
     );
-  }
 
-  // ethers is available in the global scope
-  const [deployer] = await ethers.getSigners();
-  console.log(
-    "Deploying the contracts with the account:",
-    await deployer.getAddress()
-  );
+    // Get the contract factory
+    const Lottery = await ethers.getContractFactory("Lottery");
 
-  console.log("Account balance:", (await deployer.getBalance()).toString());
+    // Deploy the contract with the specified parameters
+    const lottery = await Lottery.deploy(
+        ticketPrice,
+        housePercentage,
+        maxWinners,
+        vrfCoordinator,
+        linkToken,
+        keyHash,
+        fee
+    );
 
-  const Token = await ethers.getContractFactory("Token");
-  const token = await Token.deploy();
-  await token.deployed();
+    // Wait for the deployment to be mined
+    await lottery.deployed();
 
-  console.log("Token address:", token.address);
-
-  // We also save the contract's artifacts and address in the frontend directory
-  saveFrontendFiles(token);
+    // Log the deployed contract address
+    console.log("Lottery contract deployed to:", lottery.address);
 }
 
-function saveFrontendFiles(token) {
-  const fs = require("fs");
-  const contractsDir = path.join(__dirname, "..", "frontend", "src", "contracts");
-
-  if (!fs.existsSync(contractsDir)) {
-    fs.mkdirSync(contractsDir);
-  }
-
-  fs.writeFileSync(
-    path.join(contractsDir, "contract-address.json"),
-    JSON.stringify({ Token: token.address }, undefined, 2)
-  );
-
-  const TokenArtifact = artifacts.readArtifactSync("Token");
-
-  fs.writeFileSync(
-    path.join(contractsDir, "Token.json"),
-    JSON.stringify(TokenArtifact, null, 2)
-  );
-}
-
+// Execute the deployment script
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
