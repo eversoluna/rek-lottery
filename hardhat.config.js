4a205ac5688f70eca2ce675099b3c1e76fbe5809
eversoluna <phan.powray@outlook.com>
2023-12-15 12:02:55 +0700

    update vrf consumer base

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,16 @@
 require("@nomiclabs/hardhat-ethers");
-require("@chainlink/contracts/truffle/VRFConsumerBase");
+require("@chainlink/contracts/src/v0.8/VRFConsumerBase.sol");
 
 module.exports = {
-  solidity: "0.8.0",
+  solidity: "0.8.13",
   networks: {
     rinkeby: {
-      url: "https://rinkeby.infura.io/v3/" + process.env.INFURA_PROJECT_ID,
-      accounts: process.env.PRIVATE_KEY
-    }
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY
+    apiKey: process.env.ETHERSCAN_API_KEY,
   },
   paths: {
     sources: "./contracts",
